145f6994d761b53951b39290e9c3f81f327f4de7
zabdu <zabdulam33r@gmail.com>
2020-11-04 12:20:38 -0500

    Clean up hacky code

--- libs/algorithms.py
+++ libs/algorithms.py
@@ -9,28 +9,16 @@
 def bubbleSort(ls):
     is_sorted = False
     while not is_sorted:
-        for i in range(len(ls)):
-            next_element = None
-            try:
-                next_element = ls[i+1]
-            except IndexError:
-                break
-            if ls[i] > next_element:
-                temp_i = int(ls[i+1])
-                ls[i+1] = int(ls[i])
-                ls[i] = temp_i
-        for i in range(len(ls)):
-            next_element = None
-            try:
-                next_element = ls[i+1]
-            except IndexError:
-                break
+        for i in range(len(ls) - 1):
+            if ls[i] > ls[i+1]:
+                ls[i+1], ls[i] = int(ls[i]), int(ls[i+1])
+        for i in range(len(ls) - 1):
             if ls[i] < ls[i+1]:
                 is_sorted = True
             else:
                 is_sorted = False
                 break
-    return ls
+    return ls.copy()
 
 def insertionSort(ls):
     return ls
