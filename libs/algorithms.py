5e9142051b424b2cabba57674751944417bfc9ce
Luke Sequeira <lukemsequeira@gmail.com>
2020-11-04 13:51:53 -0500

    Added the 3 algorithms

--- libs/algorithms.py
+++ libs/algorithms.py
@@ -6,19 +6,37 @@
 #
 
 
-def bubbleSort(lst):
+def bubbleSort(a):
+    lst = a.copy()
     lnt = len(lst)
     for i in range(lnt):
         for j in range(lnt - 1):
             if(lst[j] >  lst[j+1]):
                 lst[j], lst[j+1] = lst[j+1], lst[j] #switch j and j + 1
-    
     return lst
 
 
-def insertionSort(lst2):
-    return lst2
+def insertionSort(a):
+    lst = a.copy()
+    lnt = len(lst)
+    for i in range(lnt-1): #loop from 2nd to last element (since list of len 1 is trivially sorted)
+        key = lst[i + 1]
+        j = i
+        while(j >= 0 and key < lst[j]):
+            lst[j + 1] = lst[j]
+            j -= 1
+        lst[j + 1] = key
+    return lst
 
 
-def selectionSort(lst3):
-    return lst3
+def selectionSort(a):
+    lst = a.copy()
+    lnt = len(lst) - 1
+    for i in range(lnt):
+        key = i
+        for j in range(i + 1, lnt + 1):
+            if(lst[j] < lst[key]):
+                key = j
+        lst[key], lst[i] = lst[i], lst[key]
+    return lst
+
---
+++ testsort.py
@@ -0,0 +1,10 @@
+from libs import algorithms as a
+b = [3, 1, 2]
+print([3, 1, 2])
+print(a.bubbleSort([3, 1, 2]))
+print(a.insertionSort([3, 1, 2]))
+print(a.selectionSort([3, 1, 2]))
+
+print(a.bubbleSort(b))
+print(a.insertionSort(b))
+print(a.selectionSort(b))
